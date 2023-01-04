675a4fba7ad12aad87005519278e5ede19bd7b60
Long Nguyen <116136795+lzn1205@users.noreply.github.com>
2023-01-03 20:04:15 -0600

    Create CountNegatives.java

---
+++ Module 1/CountNegatives.java
@@ -0,0 +1,18 @@
+/**
+ * Applies the linear scan strategy to counting the number of negative
+ * values in an array.
+ */
+public class CountNegatives {
+	/**
+     * Returns the number of negative values in the given array.
+     */
+	public static int countNegatives(int[]a) {
+		int negativeCount = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] < 0) {
+				negativeCount++;
+			}
+		}
+		return negativeCount;
+	}
+}
